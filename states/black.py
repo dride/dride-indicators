23636e9ccdc46512f4cf2e140da6249830453e37
saoron <yossi@yapq.com>
2017-01-17 14:41:26 +0200

    add states

---
+++ states/black.py
@@ -0,0 +1,71 @@
+#Need neopixel
+
+import time
+
+from neopixel import *
+
+LED_COUNT       = 16
+LED_PIN         = 12 #12 / 18
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 30     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal
+
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+        """Wipe color across display a pixel at a time."""
+        for i in range(strip.numPixels()):
+                strip.setPixelColor(i, color)
+                strip.show()
+                time.sleep(wait_ms/1000.0)
+
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+        """Movie theater light style chaser animation."""
+        for j in range(iterations):
+                for q in range(3):
+                        for i in range(0, strip.numPixels(), 3):
+                                strip.setPixelColor(i+q, color)
+                        strip.show()
+                        time.sleep(wait_ms/1000.0)
+                        for i in range(0, strip.numPixels(), 3):
+                                strip.setPixelColor(i+q, 0)
+
+
+def rainbowCycle(strip, wait_ms=20, iterations=5):
+        """Draw rainbow that uniformly distributes itself across all pixels."""
+        for j in range(256*iterations):
+                for i in range(strip.numPixels()):
+                        strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+                strip.show()
+                time.sleep(wait_ms/1000.0)
+def wheel(pos):
+        """Generate rainbow colors across 0-255 positions."""
+        if pos < 85:
+                return Color(pos * 3, 255 - pos * 3, 0)
+        elif pos < 170:
+                pos -= 85
+                return Color(255 - pos * 3, 0, pos * 3)
+        else:
+                pos -= 170
+                return Color(0, pos * 3, 255 - pos * 3)
+
+def dimColor (color):
+        """ Color is an 32-bit int that merges the values into one """
+        return (((color&0xff0000)/3)&0xff0000) + (((color&0x00ff00)/3)&0x00ff00) + (((color&0x0000ff)/3)&0x0000ff)
+
+if __name__ == '__main__':
+        # Create neopixel object
+        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+        strip.begin()
+
+        print 'Press Ctrl-c to quit.'
+
+        while(True):
+                # colorWipe(strip, Color(255, 255, 255))  # white wipe
+                # colorWipe(strip, Color(0, 0, 0))  # white wipe
+
+
+                theaterChase(strip, Color(0,   0,   0))  # Red theater chase
+        # colorWipe (stick,Color(0,0,0)) #reset
+
